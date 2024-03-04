dadacf37af51a5c8bf724582b9001641797e8be0
zambrix20 <companero20112011@hotmail.com>
2024-03-03 23:16:20 -0600

    Update routes and add login functionality, routes protected

--- DesafioReact/src/App.jsx
+++ DesafioReact/src/App.jsx
@@ -1,24 +1,33 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { React, useState } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { React, useEffect, useState } from 'react'
 import Principal from './Pages/Principal'
 import NewPost from './Pages/NewPost'
 import PostDetail from './Pages/PostDetail'
 import Register from './Pages/UserRegister'
 // import LoginForm from './Pages/UserLogin'
+// import { useNavigate } from 'react-router-dom'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    // Verifica la existencia de un token válido en el localStorage
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Principal />} />
+        <Route path="/" element={isLoggedIn ? <Principal /> : <Navigate to="/login" />} />
         <Route path="/login" element={<Register />} />
-        <Route path="/newpost" element={<NewPost />} />
-        <Route path='/postDetail/:id' element={<PostDetail />} />
+        <Route path="/newpost" element={isLoggedIn ? <NewPost /> : <Navigate to="/login" />} />
+        <Route path='/postDetail/:id' element={isLoggedIn ? <PostDetail /> : <Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
