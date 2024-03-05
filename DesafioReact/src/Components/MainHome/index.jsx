08668b5507a70fa1157f61a3b43c0867dd9f57c1
geranhuns <gera.nuncio@gmail.com>
2024-03-04 23:12:51 -0600

    added states to filterNav home

--- DesafioReact/src/Components/MainHome/index.jsx
+++ DesafioReact/src/Components/MainHome/index.jsx
@@ -5,29 +5,33 @@ import PostCard from '../PostCard'
 
 export default function MainHome(props) {
     const [posts, setPosts] = useState([])
-    const[postTitleToSearch,setpostTitleToSearch] = useState("")
-    const [filteredPost, setFilteredPost] = useState([]); //mapear este para mostrar
+    const [postTitleToSearch, setpostTitleToSearch] = useState("")
+    const [filteredPost, setFilteredPost] = useState([]);
+    const [postNavFilter, setPostNavFilter] = useState("Relevant") //mapear este para mostrar
 
-     useEffect(() => {
+    useEffect(() => {
         setpostTitleToSearch(props.postTitleB || "");
     }, [props.postTitleB]);
 
-    useEffect(()=>{
+    useEffect(() => {
         if (postTitleToSearch === "") {
             setFilteredPost([]);
             console.log('filtro vacio') // Si no hay valor, mostrar todos los trabajos
-            
-        }else{
+
+        }
+
+        else {
             const filtered = posts.filter(post => {
                 //return console.log(post.title)
                 return post.title.toLowerCase().includes(postTitleToSearch.toLowerCase());
             });
-            setFilteredPost(filtered);  
-            console.log(filteredPost) 
+            setFilteredPost(filtered);
+            console.log(filteredPost)
         }
-               
-        
-    },[postTitleToSearch,posts])
+
+
+
+    }, [postTitleToSearch, posts])
 
     // useEffect(()=>{
     //     if(filteredPost.length===0){
@@ -35,10 +39,42 @@ export default function MainHome(props) {
     //     }
     // },[filteredPost])
 
-    
 
-    
-   
+    useEffect(() => {
+        const todayTimestamp = new Date();
+
+
+        if (postNavFilter === "Latest") {
+            const filtered = posts.filter(post => {
+                const postDate = post.date
+                console.log(postDate)
+                // console.log(postDate)
+                // console.log(todayDate)
+                //return console.log(post.title)
+                const equal = (postDate === todayTimestamp)
+                console.log(equal)
+                return equal;
+            });
+            setFilteredPost(filtered);
+            console.log(filteredPost)
+
+        } else if (postNavFilter === "Top") {
+            const filtered = posts.filter(post => post.relevant === true);
+            ;
+            setFilteredPost(filtered);
+            console.log(filteredPost)
+
+        } else if (postNavFilter === "Relevant") {
+
+            setFilteredPost([]);
+            console.log(filteredPost)
+
+        }
+
+
+    }, [postNavFilter, posts])
+
+
 
 
     useEffect(() => {
@@ -58,34 +94,36 @@ export default function MainHome(props) {
         )
     }
 
-    
+
 
     return (
-        <main className="text-black">
+        <main className="text-black ">
             <header>
                 <nav className="text-black items-start h-15">
                     <ul className="flex flex-row text-lg">
-                        <li className="hover:bg-white/50 hover:text-blue-700 p-3">
+                        <li className="hover:bg-white/50 hover:text-blue-700 p-3" onClick={() => { setPostNavFilter("Relevant") }}>
                             <Link to="#" className="font-bold">
                                 Relevant
                             </Link>
                         </li>
-                        <li className="hover:bg-white/50 hover:text-blue-700 p-3">
-                            <Link to="https://dev.to/latest">Latest</Link>
+                        <li className="hover:bg-white/50 hover:text-blue-700 p-3" onClick={() => { setPostNavFilter("Latest") }}>
+                            <Link to="">Latest</Link>
                         </li>
-                        <li className="hover:bg-white/50 hover:text-blue-700 p-3">
-                            <Link to="https://dev.to/top/week">Top</Link>
+                        <li className="hover:bg-white/50 hover:text-blue-700 p-3" onClick={() => { setPostNavFilter("Top") }}>
+                            <Link to="">Top</Link>
                         </li>
                     </ul>
                 </nav>
             </header>
-           
-             {filteredPost.length === 0 && postTitleToSearch !== "" && (
-                <div>No se encontraron resultados</div>
-             )}
-            {(filteredPost.length > 0 ? filteredPost : posts).map(post => (
-                <PostCard postData={post} key={post._id} />
-            ))}
+            <div className='flex flex-col'>
+
+                {filteredPost.length === 0 && postTitleToSearch !== "" && (
+                    <div>No se encontraron resultados</div>
+                )}
+                {(filteredPost.length > 0 ? filteredPost : posts).map(post => (
+                    <PostCard postData={post} key={post._id} />
+                ))}
+            </div>
 
 
 
